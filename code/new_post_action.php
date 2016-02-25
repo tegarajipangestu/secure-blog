df17127f283f68ad29758e0e77f1885ac2eb0ed3
tegarajipangestu <13512061@std.stei.itb.ac.id>
2016-02-25 23:04:10 +0700

    Fixing upload file

--- code/new_post_action.php
+++ code/new_post_action.php
@@ -40,29 +40,29 @@ if (isset($_SESSION["isLogin"]) && (isset($_POST['csrf_token']) && $_POST['csrf_
 	} else {
 	    if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
 	        echo "The file ". basename( $_FILES["image"]["name"]). " has been uploaded.<br>";
+			$con = phpsqlconnection();
+
+			$stmt = $con->prepare("INSERT INTO post (Post_Id, Creator_Id, Title, Date, Contents, Image) 
+				VALUES (NULL,?,?,?,?,?)");
+			$stmt->bind_param('issss', $creatorid, $Judul, $Tanggal, $Konten, $target_file);
+			$stmt->execute();
+			// $result = $stmt->get_result();
+
+			// $sql = "INSERT INTO post (Post_Id, Creator_Id, Title, Date, Contents, Image) 
+			// 	VALUES (NULL".",".$creatorid.","."'".$Judul."'".","."'".$Tanggal."'".","."'".$Konten."'".","."'".$target_file."')";
+			if ($stmt->execute()) {
+				// echo "Huba";
+			   	header("Location: index.php");
+			} else {
+			    echo "Error: " . $sql . "<br>" . mysqli_error($con);
+			}
+
+			die();
+
 	    } else {
 	        echo "Sorry, there was an error uploading your file.<br>";
 	    }
 	}
-
-	$con = phpsqlconnection();
-
-	$stmt = $con->prepare("INSERT INTO post (Post_Id, Creator_Id, Title, Date, Contents, Image) 
-		VALUES (NULL,?,?,?,?,?)");
-	$stmt->bind_param('issss', $creatorid, $Judul, $Tanggal, $Konten, $target_file);
-	$stmt->execute();
-	// $result = $stmt->get_result();
-
-	// $sql = "INSERT INTO post (Post_Id, Creator_Id, Title, Date, Contents, Image) 
-	// 	VALUES (NULL".",".$creatorid.","."'".$Judul."'".","."'".$Tanggal."'".","."'".$Konten."'".","."'".$target_file."')";
-	if ($stmt->execute()) {
-		// echo "Huba";
-	   	header("Location: index.php");
-	} else {
-	    echo "Error: " . $sql . "<br>" . mysqli_error($con);
-	}
-
-	die();
 
 }else{
     header("Location: login.php"); /* Redirect browser */
